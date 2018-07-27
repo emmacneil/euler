eca7c35eb44d971f57a960c9b74dd3688b99b1f2
emmacneil <emac021@gmail.com>
2018-07-26 20:29:01 -0600

    First commit on branch tools.

---
+++ include/output.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+// Overload the << operator for vectors. Example:
+//   vector<string> v = {"hello", "world"}
+//   cout << v << endl;                    // output: {hello, world}
+template <class T>
+std::ostream & operator << (std::ostream & os, const std::vector<T> v)
+{
+  os << "{";
+  if (v.size() > 0)
+    os << v[0];
+  for (int i = 1; i < v.size(); i++)
+    os << ", " << v[i];
+  os << "}";
+  return os;
+}
+
