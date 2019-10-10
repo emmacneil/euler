e25c64d4ff4f92f7458019a1d9833316ab90ea1e
Evan <evan@cs-gb3khv2.cs.ucalgary.ca>
2019-10-09 22:13:19 -0600

    Add templated prime sieving function
    Based on the Sieve of Eratosthenes

--- include/nt.hpp
+++ include/nt.hpp
@@ -55,4 +55,28 @@ template <class T> T divisor_sum(const T n, const int x = 1)
   return ret;
 }
 
+
+
+// Sieve of Eratosthenes.
+// Returns a list of all prime numbers less than or equal to the given bound.
+// This function is templated such that it works for any integral type,
+// int, unsigned char, boost::uint128_t, etc.
+template <class T> typename std::enable_if<std::is_integral<T>::value, std::vector<T>>::type
+prime_sieve(T bound)
+{
+  std::vector<bool> sieve(bound - 1, true);
+  std::vector<T> ret;
+
+  for (T i = 0; i < bound - 1; i++)
+  {
+    if (sieve[i])
+    {
+      T p = i + 2;
+      ret.push_back(p);
+      for (T j = i + p; j < bound - 1; j += p)
+        sieve[j] = false;
+    }
+  }
+  return ret;
+}
 } // namespace
